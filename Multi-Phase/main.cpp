b59381c26f5bd029ef93649f1e288463ba8a1f4a
NecoWang <76930516@qq.com>
2017-06-30 13:51:32 +0800

    Initial Commit

---
+++ Multi-Phase/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Multi-Phase
+//
+//  Created by ncut on 17/6/30.
+//  Copyright (c) 2017年 ncut. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
